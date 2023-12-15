6b65748c147eed9d59dc38a2ea1565f5fbcb56cc
Hardik Khimsuriya <hardik.khimsuriya112978@marwadiuniversity.ac.in>
2023-12-15 21:38:23 +0530

    DAY-2

---
+++ Easy_Solutions/242.Valid Anagram-neetcode.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        Hash1={}
+        Hash2={}
+        for i in range(len(s)):
+            Hash1[s[i]]=1+Hash1.get(s[i],0)
+            Hash2[t[i]]=1+Hash2.get(t[i],0)
+        for j in range(len(s)):
+            if Hash1[c]!=Hash2.get(c,0):
+                return False
+        return True
+S1=Solution()
+S1.isAnagram("anagram","nagaram")         
---
+++ Easy_Solutions/242.Valid Anagram.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)==len(t):
+            Hash1={}
+            Hash2={}
+            for i in range(0,len(s)):
+               
+                if  s[i] in Hash1:
+                    Hash1[s[i]]+=1
+                    
+                if t[i] in Hash2:
+                    Hash2[t[i]]+=1 
+                
+                else:
+                    Hash1[s[i]]=1
+                    Hash2[t[i]]=1
+                
+ 
+            Hash1 = dict(sorted(Hash1.items()))
+            Hash2 = dict(sorted(Hash2.items()))
+            
+            if Hash1==Hash2:
+                return True
+            else: 
+                return False
+        else:
+            return False        
+S1=Solution()
+S1.isAnagram("anagram","nagaram")         
+
+            
+            
+        
